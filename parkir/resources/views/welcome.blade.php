42467f4b5980d076cc38866a47cab46932ff3ecd
zulhisyam03 <zul0342@gmail.com>
2022-11-23 18:19:11 +0800

    .

--- parkir/resources/views/welcome.blade.php
+++ parkir/resources/views/welcome.blade.php
@@ -7,7 +7,6 @@
         </div>
     @endif
     <div class="blok">
-<<<<<<< HEAD
         <div class="container">
             <div class="card">
                 <div class="card-title mt-3" align="center">
@@ -41,23 +40,6 @@
                                     </div>
                                 @enderror
                             </div>
-=======
-        <div class="card">
-            <div class="card-title mt-3" align="center">
-                <h2>Form Checkin Parkir</h2>
-                <hr>
-            </div>
-            <div class="card-body">                                
-                <form action="/checkin" method="POST">
-                    @csrf
-                    <div class="row mb-2">
-                        <div class="col-sm-4 col-form-label"><strong>No. Polisi</strong></div>
-                        <div class="col-sm"><input type="text" name="noPolisi" class="form-control text-uppercase"></div>
-                    </div>
-                    <div class="row mb-2">
-                        <div class="col-sm-4 col-form-label"><strong>Jenis Kendaraan</strong></div>
-                        <div class="col-sm"><input type="text" name="jenisKendaraan" class="form-control text-capitalize">
->>>>>>> 1674129855f5c81a2c8166aa6aab72467275df14
                         </div>
                         <button class="btn btn-success" style="float:right;">Checkin</button>
                     </form>
